208b8e0c36d5a001c11ce4c2e6ba1817b5f19bca
Tatiana Meshcheryakova <tania.meshcher@gmail.com>
2017-11-21 23:29:00 +0100

    reformatting and minor refactoring

--- get_from_google_api.py
+++ get_from_google_api.py
@@ -1,34 +1,35 @@
-
 import datetime
-import requests
-#import dateutil.parser
 
-def from_utc(utcTime,fmt="%Y-%m-%dT%H:%M:%fZ"):
+import requests
+
+
+def from_utc(utcTime, fmt="%Y-%m-%dT%H:%M:%fZ"):
     """
     Convert UTC time string to time.struct_time
     """
     # change datetime.datetime to time, return time.struct_time type
-    
-    return ((datetime.datetime.strptime(utcTime, fmt)))#. .replace(second=0, microsecond=0))
 
-#printing title, Posted datetime and description    
+    return datetime.datetime.strptime(utcTime, fmt)  # . .replace(second=0, microsecond=0))
+
+
+# printing title, Posted datetime and description
 response = requests.get('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=edf78a83063943ffabc69e15c78e2f93')
 data = response.json()
 
 for article in data["articles"]:
     publishedAt = str(article["publishedAt"])
-    timestamp = from_utc(publishedAt) #dateutil.parser.parse(article["publishedAt"])
-    timeToPrint = '{}-{}-{} {}:{}'.format(timestamp.day, timestamp.month, timestamp.year, timestamp.hour, timestamp.minute)
-    print ("------------------------")
-    print ("Title:\t\t", article["title"], "\nPosted on:\t", timeToPrint)
-    print ("Text:\t\t", article["description"])
-print ("------------------------")
+    timestamp = from_utc(publishedAt)
+    timeToPrint = '{}-{}-{} {}:{}'.format(timestamp.day, timestamp.month, timestamp.year, timestamp.hour,
+                                          timestamp.minute)
+    print("------------------------")
+    print("Title:\t\t", article["title"], "\nPosted on:\t", timeToPrint)
+    print("Text:\t\t", article["description"])
+print("------------------------")
 
-print (type(data))
+print(type(data))
 
 
 
 
-#from_utc("2007-03-04T21:08:12.123Z")
-#gt(data["articles"][0]["publishedAt"])
-
+# from_utc("2007-03-04T21:08:12.123Z")
+# gt(data["articles"][0]["publishedAt"])
